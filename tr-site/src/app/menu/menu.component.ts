93ab4ff72f5f80e699ada9d319e96db05660f6c9
Carlos Chitty <chitty.carlosj@gmail.com>
2017-02-10 17:10:31 -0500

    Use trash radar logo

--- tr-site/src/app/menu/menu.component.ts
+++ tr-site/src/app/menu/menu.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  public trashRadarLogo = 'assets/img/trash-radar-logo.png';
+
   constructor() { }
 
   ngOnInit() {
