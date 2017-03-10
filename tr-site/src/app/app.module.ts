3c0209d4a094112261697483f154e265f92daedb
Carlos Chitty <chitty.carlosj@gmail.com>
2017-03-10 11:56:00 -0500

    Split landing page into home and landing

--- tr-site/src/app/app.module.ts
+++ tr-site/src/app/app.module.ts
@@ -7,18 +7,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    LandingComponent
+    LandingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
